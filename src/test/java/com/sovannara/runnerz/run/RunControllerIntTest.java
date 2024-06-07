1244cd008c3ebd3ceb63b0a05309f888f130cf6f
sovannarapov <sovannarapov9@gmail.com>
2024-06-07 12:07:43 +0700

    feat: :sparkles: complete implement RunController test

--- src/test/java/com/sovannara/runnerz/run/RunControllerIntTest.java
+++ src/test/java/com/sovannara/runnerz/run/RunControllerIntTest.java
@@ -5,14 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClient;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class RunControllerTest {
+class RunControllerIntTest {
     @LocalServerPort
     int randomServerPort;
 
@@ -35,10 +37,63 @@ class RunControllerTest {
 
     @Test
     void shouldFindRunById() {
-        Object run = restClient.get()
+        Run run = restClient.get()
                 .uri("/api/runs/1")
                 .retrieve()
-                .body(new ParameterizedTypeReference<>() {});
-        System.out.println("RUN >>>>>>> " + run.getClass().);
+                .body(Run.class);
+        assertNotNull(run);
+        assertAll(
+                () -> assertEquals(1, run.id()),
+                () -> assertEquals("Noon Run", run.title()),
+                () -> assertEquals("2024-02-20T06:05", run.startedOn().toString()),
+                () -> assertEquals("2024-02-20T10:27", run.completedOn().toString()),
+                () -> assertEquals(24, run.miles()),
+                () -> assertEquals(Location.INDOOR, run.location())
+        );
+    }
+
+    @Test
+    void shouldCreateNewRun() {
+        Run run = new Run(
+                11,
+                "Saturday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(2),
+                10,
+                Location.OUTDOOR
+        );
+        ResponseEntity<Void> newRun = restClient.post()
+                .uri("/api/runs")
+                .body(run)
+                .retrieve()
+                .toBodilessEntity();
+        assertEquals(201, newRun.getStatusCode().value());
+    }
+
+    @Test
+    void shouldUpdateRun() {
+        Run run = new Run(
+                11,
+                "Updated Saturday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(3),
+                11,
+                Location.INDOOR
+        );
+        ResponseEntity<Void> updatedRun = restClient.put()
+                .uri("/api/runs/11")
+                .body(run)
+                .retrieve()
+                .toBodilessEntity();
+        assertEquals(204, updatedRun.getStatusCode().value());
+    }
+
+    @Test
+    void shouldDeleteRun() {
+        ResponseEntity<Void> deletedRun = restClient.delete()
+                .uri("/api/runs/11")
+                .retrieve()
+                .toBodilessEntity();
+        assertEquals(204, deletedRun.getStatusCode().value());
     }
 }
